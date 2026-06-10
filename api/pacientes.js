1fc4f7343e13f14d7c71a3c15bb2eda9348ce8b4
FISIO365 <SOMOSFISIO365@GMAIL.COM>
2026-06-10 18:29:27 +0200

    Update pacientes.js

--- api/pacientes.js
+++ api/pacientes.js
@@ -1,4 +1,4 @@
-export const config = { runtime: 'edge' };
+export const config = { runtime: 'nodejs', maxDuration: 60 };
 
 const AIRTABLE_TOKEN = process.env.AIRTABLE_TOKEN;
 const FISIO_PASSWORD = process.env.FISIO_PASSWORD || 'FISIO365App';
